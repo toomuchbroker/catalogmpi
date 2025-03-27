f48feb0447618692f4bcb48c60300f73aec76323
broxielena <147066495+broxielena@users.noreply.github.com>
2025-03-27 17:11:08 +0200

    2nd line output test

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello, this is a test message!");
 
+        System.out.println("This is a second line of output.");
     }
 }
