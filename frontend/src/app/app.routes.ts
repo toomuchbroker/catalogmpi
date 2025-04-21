c299c8f320878b35d187cfe5ba5d8da2743670fc
broxielena <147066495+broxielena@users.noreply.github.com>
2025-04-21 22:53:37 +0300

    styling for admin panel and user table

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './login/login.component';
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
 
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: '', redirectTo: 'admin-panel', pathMatch: 'full' },
     { path: 'signup', loadComponent: () => import('./signup/signup.component').then(m => m.SignupComponent) },
     { path: 'admin-panel', loadComponent: () => import('./admin-panel/admin-panel.component').then(m => m.AdminPanelComponent) },
     //{ path: 'user-panel', loadComponent: () => import('./user-panel/user-panel.component').then(m => m.UserPanelComponent)}
