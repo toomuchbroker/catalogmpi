781ea34b6826866aa30e4bfe85383203eb54fb9b
Erik <doodletvm@gmail.com>
2025-04-22 16:30:04 +0300

    add info bar to admin panel

--- frontend/src/app/admin-panel/admin-panel.component.ts
+++ frontend/src/app/admin-panel/admin-panel.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserTableComponent } from '../user-table/user-table.component';
 import { AddUserComponent } from '../add-user/add-user.component';
+import { InfoBarComponent } from "../info-bar/info-bar.component";
 
 @Component({
   selector: 'app-admin-panel',
   standalone: true,
-  imports: [CommonModule, AddUserComponent, UserTableComponent],
+  imports: [CommonModule, AddUserComponent, UserTableComponent, InfoBarComponent],
   templateUrl: './admin-panel.component.html',
   styleUrls: ['./admin-panel.component.css']
 })
